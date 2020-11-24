4c89c279f0c10389f856fdaf3de1839e22ba302d
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2020-11-24 19:55:09 +0200

    added solution of the task-10

---
+++ js/task-10.js
@@ -0,0 +1,15 @@
+// Получить массив всех умений всех пользователей(поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+import { default as users } from './users.js'
+
+const getSortedUniqueSkills = users => {
+    const skillsArray = users.reduce((acc, user) => {
+        acc.push(...user.skills);
+
+        return acc;
+    }, [])
+
+    return [...new Set(skillsArray)].sort()
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
